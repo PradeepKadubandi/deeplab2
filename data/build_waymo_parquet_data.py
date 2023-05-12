946efbec2c449724bebb512240854927fd889d9d
PradeepKadubandi <pradeep.v.kadubandi@gmail.com>
2023-05-12 11:57:02 -0700

    First version of script that generates data in vip-deeplab format (next frame).

--- data/build_waymo_parquet_data.py
+++ data/build_waymo_parquet_data.py
@@ -17,7 +17,7 @@ r"""Converts Waymo Parquet data to sharded TFRecord file format with tf.train.Ex
 
 The expected directory structure of the step_root parameter should be as follows:
 
-  + step_root
+  + root_folder
     + camera_image
       + *.parquet
     + camera_segmentation
@@ -38,20 +38,29 @@ The output Example proto contains the following fields:
   image/channels: image channels.
   image/segmentation/class/encoded: encoded panoptic segmentation content.
   image/segmentation/class/format: segmentation encoding format.
+  image/depth/encoded: encoded depth content.
+  image/depth/format: depth encoding format.
   video/sequence_id: sequence ID of the frame.
   video/frame_id: ID of the frame of the video sequence.
+  next_image/encoded: encoded next-frame image content.
+  next_image/segmentation/class/encoded: encoded panoptic segmentation content
+    of the next frame.
 
 Example to run the scipt:
 
-   python -m deeplab2.data.build_waymo_parquet_data.py \
-     --step_root=${STEP_ROOT} \
-     --output_dir=${OUTPUT_DIR}
+   python -m deeplab2.data.build_waymo_parquet_data \
+     --root_dir=${ROOT_DIR} \
+     --output_dir=${OUTPUT_DIR} \
+     --is_training_data=${IS_TRAINING_DATA} \
+     --is_testing_data=${IS_TESTING_DATA}
 """
 
+import asyncio
 import math
 import os
 
 from typing import Iterator, List, Sequence, Tuple, Optional, Union
+from collections import OrderedDict
 
 from absl import app
 from absl import flags
@@ -64,155 +73,209 @@ from PIL import Image
 
 import tensorflow as tf
 import dask.dataframe as dd
+import pandas as pd
 from waymo_open_dataset import v2
+from waymo_open_dataset.utils import camera_segmentation_utils
 
 from deeplab2.data import data_utils
 
 FLAGS = flags.FLAGS
 
-flags.DEFINE_string('step_root', None, 'STEP dataset root folder.')
+flags.DEFINE_string('root_dir', None, 'Url or path of the root directory.')
 
 flags.DEFINE_string('output_dir', None,
                     'Path to save converted TFRecord of TensorFlow examples.')
 flags.DEFINE_bool(
-    'use_two_frames', False, 'Flag to separate between 1 frame '
-    'per TFExample or 2 consecutive frames per TFExample.')
+    'is_training_data', None, 'Flag to specify if the data is training data.')
+
+flags.DEFINE_bool(
+    'is_testing_data', None, 'Flag to specify if the data is testing data (no labels available).')
 
 _PANOPTIC_LABEL_FORMAT = 'png'
-# _NUM_SHARDS = 10
-# _IMAGE_FOLDER_NAME = 'images'
-# _PANOPTIC_MAP_FOLDER_NAME = 'panoptic_maps'
-# _LABEL_MAP_FORMAT = 'png'
-# _INSTANCE_LABEL_DIVISOR = 1000
-# _ENCODED_INSTANCE_LABEL_DIVISOR = 256
 _TF_RECORD_PATTERN = '%s.tfrecord'
-# _FRAME_ID_PATTERN = '%06d'
 
-def get_context_names(step_root: str, tag: str) -> List[str]:
-  paths = tf.io.gfile.glob(f'{step_root}/{tag}/*.parquet')
+def get_context_names(root_dir: str, tag: str) -> List[str]:
+  """Gets the path names for the camera images and panoptic maps."""
+  paths = tf.io.gfile.glob(f'{root_dir}/{tag}/*.parquet')
   return list(map(lambda path: os.path.splitext(os.path.basename(path))[0], paths))
 
-def read(step_root: str, tag: str) -> dd.DataFrame:
+def read(root_dir: str, tag: str, context_name: str) -> dd.DataFrame:
   """Creates a Dask DataFrame for the component specified by its tag."""
-  paths = tf.io.gfile.glob(f'{step_root}/{tag}/*.parquet')
-  return dd.read_parquet(paths)
+  paths = tf.io.gfile.glob(f'{root_dir}/{tag}/{context_name}.parquet')
+  return pd.read_parquet(paths)
 
-# def _get_previous_frame_path(image_path: str) -> str:
-#   """Gets previous frame path. If not exists, duplicate it with image_path."""
-#   frame_id, frame_ext = os.path.splitext(os.path.basename(image_path))
-#   folder_dir = os.path.dirname(image_path)
-#   prev_frame_id = _FRAME_ID_PATTERN % (int(frame_id) - 1)
-#   prev_image_path = os.path.join(folder_dir, prev_frame_id + frame_ext)
-#   # If first frame, duplicates it.
-#   if not tf.io.gfile.exists(prev_image_path):
-#     tf.compat.v1.logging.warn(
-#         'Could not find previous frame %s of frame %d, duplicate the previous '
-#         'frame with the current frame.', prev_image_path, int(frame_id))
-#     prev_image_path = image_path
-#   return prev_image_path
+# def _create_panoptic_tfexample(dataframe_row: dd.Series,
+#                                use_two_frames: bool,
+#                                is_testing: bool = False) -> tf.train.Example:
+#   """Creates a TF example for each image.
 
+#   Args:
+#     image_path: Path to the image.
+#     panoptic_map_path: Path to the panoptic map (as an image file).
+#     use_two_frames: Whether to encode consecutive two frames in the Example.
+#     is_testing: Whether it is testing data. If so, skip adding label data.
 
-def _create_panoptic_tfexample(dataframe_row: dd.Series,
-                               use_two_frames: bool,
-                               is_testing: bool = False) -> tf.train.Example:
-  """Creates a TF example for each image.
+#   Returns:
+#     TF example proto.
+#   """
+#   cam_image = v2.CameraImageComponent.from_dict(dataframe_row)
+#   cam_seg_label = v2.CameraSegmentationLabelComponent.from_dict(dataframe_row)
+#   image_data = cam_image.image
 
-  Args:
-    image_path: Path to the image.
-    panoptic_map_path: Path to the panoptic map (as an image file).
-    use_two_frames: Whether to encode consecutive two frames in the Example.
-    is_testing: Whether it is testing data. If so, skip adding label data.
-
-  Returns:
-    TF example proto.
-  """
-  cam_image = v2.CameraImageComponent.from_dict(dataframe_row)
-  cam_seg_label = v2.CameraSegmentationLabelComponent.from_dict(dataframe_row)
-  image_data = cam_image.image
-
-  label_data = None
-  if not is_testing:
-    label_data = cam_seg_label.panoptic_label
-  image_name = "How-is-this-used?"
-  image_format = "jpeg"
-  sequence_id = cam_seg_label.sequence_id 
-  frame_id = str(cam_image.key.camera_name)
-  prev_image_data = None
-  prev_label_data = None
-  # if use_two_frames:
-  #   # Previous image.
-  #   prev_image_path = _get_previous_frame_path(image_path)
-  #   with tf.io.gfile.GFile(prev_image_path, 'rb') as f:
-  #     prev_image_data = f.read()
-  #   # Previous panoptic map.
-  #   if not is_testing:
-  #     prev_panoptic_map_path = _get_previous_frame_path(panoptic_map_path)
-  #     prev_label_data = _decode_panoptic_map(prev_panoptic_map_path)
-  return data_utils.create_video_tfexample(
-      image_data,
-      image_format,
-      image_name,
-      label_format=_PANOPTIC_LABEL_FORMAT,
-      sequence_id=sequence_id,
-      image_id=frame_id,
-      label_data=label_data,
-      prev_image_data=prev_image_data,
-      prev_label_data=prev_label_data)
+#   label_data = None
+#   if not is_testing:
+#     label_data = cam_seg_label.panoptic_label
+#   image_name = "How-is-this-used?"
+#   image_format = "jpeg"
+#   sequence_id = cam_seg_label.sequence_id 
+#   frame_id = str(cam_image.key.camera_name)
+#   next_image_data = None
+#   next_label_data = None
+#   return data_utils.create_video_and_depth_tfexample(
+#       image_data,
+#       image_format,
+#       image_name,
+#       label_format=_PANOPTIC_LABEL_FORMAT,
+#       sequence_id=sequence_id,
+#       image_id=frame_id,
+#       label_data=label_data,
+#       next_image_data=next_image_data,
+#       next_label_data=next_label_data)
 
 
-def _convert_dataset(step_root: str,
-                     dataset_split: str,
+def _convert_dataset(root_dir: str,
                      output_dir: str,
-                     use_two_frames: bool = False):
+                     is_training_data: bool,
+                     is_testing_data: bool):
   """Converts the specified dataset split to TFRecord format.
 
   Args:
-    step_root: String, Path to STEP dataset root folder.
-    dataset_split: String, the dataset split (e.g., train, val).
+    root_dir: String, Url of directory containing the component directories.
     output_dir: String, directory to write output TFRecords to.
-    use_two_frames: Whether to encode consecutive two frames in the Example.
+    is_training_data: Boolean, whether the data is for training.
+    is_testing_data: Boolean, whether the data is for testing.
   """
-  # For val and test set, if we run with use_two_frames, we should create a
-  # sorted tfrecord per sequence.
-  create_tfrecord_per_sequence = ('train'
-                                  not in dataset_split) and use_two_frames
-  is_testing = 'test' in dataset_split
-
-  cam_image_df = read(step_root=step_root, tag='camera_image')
-  cam_seg_df = read(step_root=step_root, tag='camera_segmentation')
-
-  image_w_seg_df = v2.merge(cam_image_df, cam_seg_df)
-
-  # This is too costly when using the cloud storage and entire dataset
-  # context_names = image_w_seg_df['key.segment_context_name'].unique().compute()
-  context_names = get_context_names(step_root, 'camera_image')
+  context_names = get_context_names(root_dir, 'camera_image')
+  print (f'Found {len(context_names)} context names.')
 
   for context_name in tqdm(context_names):
-    group = image_w_seg_df[image_w_seg_df['key.segment_context_name'] == context_name]
-    shard_filename = _TF_RECORD_PATTERN % (context_name)
+    process_context_name(root_dir=root_dir, 
+                         output_dir=output_dir, 
+                         context_name=context_name, 
+                         is_for_training=is_training_data, 
+                         is_for_testing=is_testing_data)
+
+def process_context_name(root_dir, output_dir, context_name, is_for_training, is_for_testing):
+    shard_filename = f'{context_name}.tfrecord'
     output_filename = os.path.join(output_dir, shard_filename)
+    if (os.path.exists(output_filename)):
+      return
+    
+    context_data = read(root_dir=root_dir, tag='camera_image', context_name=context_name)    
+    if not is_for_testing:
+        cam_seg_df = read(root_dir=root_dir, tag='camera_segmentation', context_name=context_name)
+        context_data = v2.merge(context_data, cam_seg_df)
+
+    tf_records = list()
+    
+    groups = context_data.groupby(["[CameraSegmentationLabelComponent].sequence_id"])
+    for sequence_id, group_by_sequence in groups:
+        timestamps = pd.unique(group_by_sequence["key.frame_timestamp_micros"].sort_values())
+        next_timestamp_map = dict(zip(timestamps[:-1], timestamps[1:]))
+        next_timestamp_map[timestamps[-1]] = timestamps[-1] # set the last timestamp as next to itself - to duplicate the last row as next frame
+        
+        image_protos = OrderedDict()
+        label_protos = OrderedDict()
+        label_values = OrderedDict()
+        
+        for (_, row) in group_by_sequence.iterrows():
+            cam_image = v2.CameraImageComponent.from_dict(row)
+            row_key = (cam_image.key.camera_name, cam_image.key.frame_timestamp_micros)
+            image_protos[row_key] = cam_image
+            if not is_for_testing:
+                cam_seg_label = v2.CameraSegmentationLabelComponent.from_dict(row)
+                label_protos[row_key] = cam_seg_label
+                
+        if not is_for_testing:
+            panoptic_labels, is_tracked_masks, num_cameras_covered, panoptic_label_divisor = camera_segmentation_utils.decode_multi_frame_panoptic_labels_from_segmentation_labels(
+                segmentation_proto_list=list(label_protos.values()),
+                remap_to_global=True,
+                remap_to_sequential=is_for_training,
+                new_panoptic_label_divisor=100000
+            )
+            for index, row_key in enumerate(label_protos):
+                label_values[row_key] = (panoptic_labels[index], is_tracked_masks[index], num_cameras_covered[index])
+                
+        for row_key, image_component in image_protos.items():
+            camera_name, curr_timestamp = row_key
+            image_data = image_component.image
+            image_format = "jpeg"
+            filename = f"context:{context_name}::sequence:{sequence_id}::camera_name:{image_component.key.camera_name}::timestamp:{image_component.key.frame_timestamp_micros}"
+            image_id = filename
+            
+            next_timestamp = next_timestamp_map[curr_timestamp]
+            next_row_key = (camera_name, next_timestamp)
+            next_image_data=image_protos[next_row_key].image
+            
+            label_data=None
+            label_format=None
+            next_label_data=None
+            
+            if not is_for_testing:
+                curr_panoptic_label, curr_is_tracked_mask, curr_num_cameras_covered = label_values[row_key]
+                next_panoptic_label, next_is_tracked_mask, next_num_cameras_covered = label_values[next_row_key]
+                label_data=curr_panoptic_label.tobytes()
+                label_format="raw"
+                next_label_data=next_panoptic_label.tobytes()
+                
+            tf_record = data_utils.create_video_and_depth_tfexample(
+                image_data=image_data,
+                image_format=image_format,
+                filename=filename,
+                label_format=label_format,
+                sequence_id=sequence_id,
+                image_id=image_id,
+                label_data=label_data,
+                next_image_data=next_image_data,
+                next_label_data=next_label_data,
+                depth_data=None,
+                depth_format=None
+            )
+            tf_records.append(tf_record)
+
     with tf.io.TFRecordWriter(output_filename) as tfrecord_writer:
-      for i, (_, dataframe_row) in enumerate(group.iterrows()):
-        example = _create_panoptic_tfexample(dataframe_row,
-                                            use_two_frames, is_testing)
-        tfrecord_writer.write(example.SerializeToString())
+        for example in tf_records:
+            tfrecord_writer.write(example.SerializeToString())
+
+# def process_context_name(step_root, output_dir, use_two_frames, is_testing, context_name):
+#     shard_filename = _TF_RECORD_PATTERN % (context_name)
+#     output_filename = os.path.join(output_dir, shard_filename)
+#     if (os.path.exists(output_filename)):
+#       return
+    
+#     cam_image_df = read(step_root=step_root, tag='camera_image', context_name=context_name)
+#     cam_seg_df = read(step_root=step_root, tag='camera_segmentation', context_name=context_name)
+
+#     image_w_seg_df = v2.merge(cam_image_df, cam_seg_df)
+
+#     with tf.io.TFRecordWriter(output_filename) as tfrecord_writer:
+#       for i, (_, dataframe_row) in enumerate(image_w_seg_df.iterrows()):
+#         example = _create_panoptic_tfexample(dataframe_row,
+#                                             use_two_frames, is_testing)
+#         tfrecord_writer.write(example.SerializeToString())
 
 
 def main(argv: Sequence[str]) -> None:
   if len(argv) > 1:
     raise app.UsageError('Too many command-line arguments.')
   tf.io.gfile.makedirs(FLAGS.output_dir)
-  for dataset_split in ['train']:
-  # for dataset_split in ('train', 'val', 'test'):
-    logging.info('Starts to processing STEP dataset split %s.', dataset_split)
-    _convert_dataset(FLAGS.step_root, dataset_split, FLAGS.output_dir,
-                     FLAGS.use_two_frames)
-
+  _convert_dataset(FLAGS.root_dir, FLAGS.output_dir,
+                    FLAGS.is_training_data, FLAGS.is_testing_data)
 
 if __name__ == '__main__':
-  cluster = LocalCluster()
-  dask_client = DDClient(cluster)
-  print (f"Dashboard link: {dask_client.dashboard_link}")
-  print ("Dask Scheduler Info: ")
-  print (dask_client.scheduler_info())
+  # cluster = LocalCluster()
+  # dask_client = DDClient(cluster)
+  # print (f"Dashboard link: {dask_client.dashboard_link}")
+  # print ("Dask Scheduler Info: ")
+  # print (dask_client.scheduler_info())
   app.run(main)
